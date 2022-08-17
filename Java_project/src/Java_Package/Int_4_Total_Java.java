8b2ca15f08ccef946919250073dc8d925c72adea
khant <khant@DESKTOP-2F5NU9C.hsd1.va.comcast.net>
2022-08-17 16:24:14 -0400

    Here,I am committing Java!

---
+++ Java_project/src/Java_Package/Int_4_Total_Java.java
@@ -0,0 +1,18 @@
+package Java_Package;
+
+public class Int_4_Total_Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a = 100; //Bucket Name a and integer number 100
+		int b= 200; //Bucket Name b and integer number 200
+		int c= 300; //Bucket Name c and integer number 300
+		int d= 400; //Bucket Name d and integer number 400
+		int Total =a+b+c+d; //It will display total of integers number
+		System.out.println("Sum of Four Number "+Total); // It will play "Sum of four number" and total of integer numbers
+		
+	}
+
+}
---
+++ Java_project/src/Java_Package/Int_5_total_java.java
@@ -0,0 +1,25 @@
+package Java_Package;
+
+public class Int_5_total_java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a= 10;
+		int b= 20;
+		int c= 30;
+		int d= 40;
+		int e= 50;
+		int total= a+b+c+d+e;
+		System.out.println("Total "+total);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
