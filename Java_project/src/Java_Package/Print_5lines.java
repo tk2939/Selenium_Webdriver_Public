a9c0ebe4704819a8c36af1385d77e7d6e5834b86
khant <khant@DESKTOP-2F5NU9C.hsd1.va.comcast.net>
2022-08-17 16:22:30 -0400

    Here,I am committing the java.

---
+++ Java_project/src/Java_Package/Print_5lines.java
@@ -0,0 +1,21 @@
+package Java_Package;
+
+public class Print_5lines {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("I am learning Java"); //This line will print "I am learning Java"
+		System.out.println("I am happy"); //This line will print "I am happy"
+		System.out.println("I am enjoying"); //This line will print I am enjoying"	
+		System.out.println("I will continue learning Java"); //This line will print I will continue learning Java
+		System.out.println("I will learn selenium");// This line will print I will learn selenium
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Java_project/src/Java_Package/Print_Hello_World.java
@@ -0,0 +1,11 @@
+package Java_Package;
+
+public class Print_Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");//This line will print "Hello World"
+
+	}
+
+}
