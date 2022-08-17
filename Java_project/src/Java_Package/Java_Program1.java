19e0f0d842b1de383409173fb3ccc1b0a8ca5696
khant <khant@DESKTOP-2F5NU9C.hsd1.va.comcast.net>
2022-08-17 16:49:03 -0400

    Here, I want committing java project!

---
+++ Java_project/src/Java_Package/Java_Program1.java
@@ -0,0 +1,13 @@
+package Java_Package;
+
+public class Java_Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+	
+		
+	}
+
+}
---
+++ Java_project/src/Java_Package/Java_program2.java
@@ -0,0 +1,17 @@
+package Java_Package;
+
+public class Java_program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Today is Sunday, July 10th, 2022");		
+		System.out.println("I am learning java for first time");
+		System.out.println("I am enjoying it");
+		
+		
+	
+
+       }
+}
